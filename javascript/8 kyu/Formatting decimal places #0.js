b76bd0d610ddcb8af0411880c271066cef9dfeda
Maciej Narejko <maciejnarejko@gmail.com>
2024-01-11 10:00:00 +0100

    ✅ Solve 8 kyu: Formatting decimal places #0

---
+++ javascript/8 kyu/Formatting decimal places #0.js
@@ -0,0 +1,9 @@
+/*
+ * Formatting decimal places #0
+ * https://www.codewars.com/kata/5641a03210e973055a00000d
+ *
+ */
+
+function twoDecimalPlaces(n) {
+	return Math.round((number + Number.EPSILON) * 100) / 100
+}
