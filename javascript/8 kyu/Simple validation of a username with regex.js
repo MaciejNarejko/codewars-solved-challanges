47fc1af353504e488bd4e4d9ac6c6bad3c63c773
Maciej Narejko <maciejnarejko@gmail.com>
2024-01-12 10:00:00 +0100

    ✅ Solve 8 kyu: Simple validation of a username with regex

---
+++ javascript/8 kyu/Simple validation of a username with regex.js
@@ -0,0 +1,9 @@
+/*
+ * Simple validation of a username with regex
+ * https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023
+ *
+ */
+
+function validateUsr(username) {
+	return /^[a-z0-9_]{4,16}$/.test(username)
+}
