60a75b3698ba4a1ec44c061c31acea943a9f4a6e
Maciej Narejko <maciejnarejko@gmail.com>
2024-01-31 10:00:00 +0100

    ✅ Solve 8 kyu: Training JS #2: Basic data types--Number

---
+++ javascript/8 kyu/Basic data types--Number.js
@@ -0,0 +1,32 @@
+/*
+ * Training JS #2: Basic data types--Number
+ * https://www.codewars.com/kata/571edd157e8954bab500032d
+ *
+ */
+
+let v1 = 50,
+	v2 = 100,
+	v3 = 150,
+	v4 = 200,
+	v5 = 2,
+	v6 = 250
+
+function equal1() {
+	return v1 + v1
+}
+
+function equal2() {
+	return v6 - v3
+}
+
+function equal3() {
+	return v1 * v5
+}
+
+function equal4() {
+	return v4 / v5
+}
+
+function equal5() {
+	return v2 % v3
+}
