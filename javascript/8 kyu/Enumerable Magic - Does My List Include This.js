7c34306df8c6ace46864aa32dd1b537966235858
Maciej Narejko <maciejnarejko@gmail.com>
2024-02-09 10:00:00 +0100

    ✅ Solve 8 kyu: Enumerable Magic - Does My List Include This?

---
+++ javascript/8 kyu/Enumerable Magic - Does My List Include This.js
@@ -0,0 +1,9 @@
+/*
+ * Enumerable Magic - Does My List Include This?
+ * https://www.codewars.com/kata/545991b4cbae2a5fda000158
+ *
+ */
+
+function include(arr, item) {
+	return arr.includes(item)
+}
