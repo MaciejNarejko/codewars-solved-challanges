f90c9d4e8a7f846b90131b5faa3213eea6c8c344
Maciej Narejko <maciejnarejko@gmail.com>
2024-02-15 10:00:00 +0100

    ✅ Solve 8 kyu: Regex count lowercase letters

---
+++ javascript/8 kyu/Regex count lowercase letters.js
@@ -0,0 +1,9 @@
+/*
+ * Regex count lowercase letters
+ * https://www.codewars.com/kata/56a946cd7bd95ccab2000055
+ *
+ */
+
+function lowercaseCount(str) {
+	return str.replace(/[^a-z]/g, '').length
+}
