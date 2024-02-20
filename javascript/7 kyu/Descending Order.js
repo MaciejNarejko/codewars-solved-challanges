7f0025b3f2dc866fb5d4725584aabe866214425c
Maciej Narejko <maciejnarejko@gmail.com>
2024-02-20 10:00:00 +0100

    ✅ Solve 7 kyu: Descending Order

---
+++ javascript/7 kyu/Descending Order.js
@@ -0,0 +1,16 @@
+/*
+ * Descending Order
+ * https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+ *
+ */
+
+function descendingOrder(n) {
+	return parseInt(n
+		.toString()
+		.split('')
+		.sort((a, b) => b - a)
+		.join('')
+		, 10
+	)
+}
+
