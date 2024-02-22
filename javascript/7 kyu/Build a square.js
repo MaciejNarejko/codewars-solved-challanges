7a2ace6b07af462ed7686fbf04a85a8fc303e70c
Maciej Narejko <maciejnarejko@gmail.com>
2024-02-22 10:00:00 +0100

    ✅ Solve 7 kyu: Build a square

---
+++ javascript/7 kyu/Build a square.js
@@ -0,0 +1,10 @@
+/*
+ * Build a square
+ * https://www.codewars.com/kata/59a96d71dbe3b06c0200009c
+ *
+ */
+
+function generateShape(integer) {
+	return Array(integer).fill('+'.repeat(integer)).join('\n')
+}
+
