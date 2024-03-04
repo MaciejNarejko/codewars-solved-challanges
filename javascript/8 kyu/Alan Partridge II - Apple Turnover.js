c649f5f492583d2a1ddf36bf36ff60faa7f019ff
Maciej Narejko <maciejnarejko@gmail.com>
2024-03-04 10:00:00 +0100

    ✅ Solve 8 kyu: Alan Partridge II - Apple Turnover

---
+++ javascript/8 kyu/Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,11 @@
+/*
+ * Alan Partridge II - Apple Turnover
+ * https://www.codewars.com/kata/580a094553bd9ec5d800007d
+ *
+ */
+
+function apple(x) {
+	return Math.pow(Number(x), 2) > 1000
+		? "It's hotter than the sun!!"
+		: 'Help yourself to a honeycomb Yorkie for the glovebox.'
+}
