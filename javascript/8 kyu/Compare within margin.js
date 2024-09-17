1595e9115279e519d1347129c508a4a6f9a5893f
Maciej Narejko <maciejnarejko@gmail.com>
2024-09-17 10:00:00 +0200

    ✅ Solve 8 kyu: Compare within margin

---
+++ javascript/8 kyu/Compare within margin.js
@@ -0,0 +1,9 @@
+/*
+ * Compare within margin
+ * https://www.codewars.com/kata/56453a12fcee9a6c4700009c
+ *
+ */
+
+function closeCompare(a, b, margin = 0) {
+	return Math.abs(a - b) <= margin ? 0 : a < b ? -1 : 1
+}
