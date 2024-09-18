96c44e54fe6dac8a97ac1294aa64b968c21b670e
Maciej Narejko <maciejnarejko@gmail.com>
2024-09-18 10:00:00 +0200

    ✅ Solve 8 kyu: Grasshopper - Combine strings

---
+++ javascript/8 kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,9 @@
+/*
+ * Grasshopper - Combine strings
+ * https://www.codewars.com/kata/55f73f66d160f1f1db000059
+ *
+ */
+
+function combineNames(firstName, lastName) {
+	return `${firstName} ${lastName}`
+}
