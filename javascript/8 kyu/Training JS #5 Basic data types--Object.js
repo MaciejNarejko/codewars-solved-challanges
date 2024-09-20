8c1f6744e2083ecb83002cf9327a5767601edbbe
Maciej Narejko <maciejnarejko@gmail.com>
2024-09-20 10:00:00 +0200

    ✅ Solve 8 kyu: Training JS #5: Basic data types--Object

---
+++ javascript/8 kyu/Training JS #5 Basic data types--Object.js
@@ -0,0 +1,9 @@
+/*
+ * Training JS #5: Basic data types--Object
+ * https://www.codewars.com/kata/571f1eb77e8954a812000837
+ *
+ */
+
+function animal(obj) {
+	return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+}
